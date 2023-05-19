96fc35b8d91e560947974a763ca617331a6d3f5e
Showman_Das <Showmandas3216@gmail.com>
2023-05-19 18:27:31 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const app = express()
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const cors=require('cors')
-require('dotenv').config()
-const port = process.env.port || 5000
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.port || 5000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mz723df.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,112 +16,142 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const musicToysCollection=client.db('music').collection('musicData')
+    const musicToysCollection = client.db("music").collection("musicData");
     const indexKeys = { toyname: 1 };
     const indexOptions = { name: "toyname" };
-    const result = await musicToysCollection.createIndex(indexKeys, indexOptions);
- 
-    // get all toy data 
-    app.get('/alltoy',async(req,res)=>{
-      const cursor =  musicToysCollection.find();
-      const result=await cursor.toArray()
+    const result = await musicToysCollection.createIndex(
+      indexKeys,
+      indexOptions
+    );
+
+    // get all toy data
+    app.get("/alltoy", async (req, res) => {
+      const cursor = musicToysCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
-     })
+    });
+
+
+    
+    app.get("/alltoy/:text", async (req, res) => {
+      console.log(req.params.text)
+      if(req.params.text =="classical" || req.params.text =="pop" || req.params.text =="hiphop" ){
+        const result=await musicToysCollection.find({status:req.params.text}).toArray()
+        return res.send(result)
+      }
+      const result=await musicToysCollection.find({}).toArray()
+      res.send(result)
+    });
+
 
     // show my toy data
-    app.get('/mytoy',async(req,res)=>{
-     console.log(req.query.email)
-     let query={}
-     if(req.query?.email){
-      query={email:req.query?.email}
-     }
-    const result=await musicToysCollection.find(query).toArray()
-    
-      res.send(result)
-      // console.log(result)
-     })
-  
+    app.get("/mytoy", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+      // const sortOption = req.query.sort === 'asc' ? 1 : -1;
+      const result = await musicToysCollection
+        .find(query)
+        // .sort({ price: sortOption })
+        .toArray();
 
-     //find one toy
-     app.get('/singletoy/:id',async(req,res)=>{
+      res.send(result);
+      // console.log(result)
+    });
+
+    //find one toy
+    app.get("/alltoy/:id", async (req, res) => {
       console.log(req.params.id);
       const singleToy = await musicToysCollection.findOne({
         _id: new ObjectId(req.params.id),
       });
       res.send(singleToy);
-     })
+    });
 
-     //insert toy data
-   app.post('/alltoy',async(req,res)=>{
-    const toyAdd=req.body;
-    console.log(toyAdd)
-    const result=await musicToysCollection.insertOne(toyAdd)
-    res.send(result)
+    //insert toy data
+    app.post("/alltoy", async (req, res) => {
+      const toyAdd = req.body;
+      console.log(toyAdd);
+      const result = await musicToysCollection.insertOne(toyAdd);
+      res.send(result);
+    });
 
-   })
+    //delete single toy data
+    app.delete("/mytoy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await musicToysCollection.deleteOne(query);
+      res.send(result);
+    });
 
-   //delete single toy data
-   app.delete('/mytoy/:id', async(req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) }
-    const result = await musicToysCollection.deleteOne(query);
-    res.send(result);
-})
+    // get specific toy's data(view details)
+    app.get("/mytoy/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await musicToysCollection.findOne(filter);
+      res.send(result);
+    });
 
-// get specific toy's data 
-app.get('/mytoy/:id',async(req,res)=>{
-  const id=req.params.id
-  const filter={_id:new ObjectId(id)}
-  const result=await musicToysCollection.findOne(filter)
-  res.send(result)
- })
+    app.get("/mytoy/:text", async (req, res) => {
+      // const sortTxt=req.params.text;
+      // const sortOption = req.query.sort === 'asc' ? 1 : -1;
+      if(req.params.text == 'ascending' || req.params.text == "descending"){
+        const result = await musicToysCollection.findOne({status:req.params.text}).sort({price:-1}).toArray()
+        console.log(result)
+        return res.send(result)
+      }
+      const result = await musicToysCollection.findOne({}).sort({price:-1}).toArray();
+      res.send(result);
+    });
 
-//  update users toy's data
-app.put('/mytoy/:id',async(req,res)=>{
-  const id=req.params.id
-  const filter={_id:new ObjectId(id)}
-  const options={upsert:true}
-  const updatedToy=req.body
-  const toy={
-    $set:{
-      toyname:updatedToy.toyname,
-      price:updatedToy.price,
-      photo:updatedToy.photo,
-      description:updatedToy.description,
-      rating:updatedToy.rating,
-      toyCategory:updatedToy.toyCategory,
-    }
-  }
-  const result=await musicToysCollection.updateOne(filter,toy,options)
-  res.send(result)
-})
-// search toy's data by name 
-app.get("/searchToys/:txt", async (req, res) => {
-  const searchToy = req.params.txt;
-  const result = await musicToysCollection
-    .find({
-       $or:[
-        {
-          toyname: { $regex: searchToy, $options: "i" } ,
 
-        }
-       ]
-      
-    })
-    .toArray();
-  res.send(result);
-});
+    //  update users toy's data
+    app.put("/mytoy/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedToy = req.body;
+      const toy = {
+        $set: {
+          toyname: updatedToy.toyname,
+          price: updatedToy.price,
+          photo: updatedToy.photo,
+          description: updatedToy.description,
+          rating: updatedToy.rating,
+          toyCategory: updatedToy.toyCategory,
+        },
+      };
+      const result = await musicToysCollection.updateOne(filter, toy, options);
+      res.send(result);
+    });
+    // search toy's data by name
+    app.get("/searchToys/:txt", async (req, res) => {
+      const searchToy = req.params.txt;
+      const result = await musicToysCollection
+        .find({
+          $or: [
+            {
+              toyname: { $regex: searchToy, $options: "i" },
+            },
+          ],
+        })
+        .toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -132,14 +159,10 @@ app.get("/searchToys/:txt", async (req, res) => {
 }
 run().catch(console.dir);
 
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Musical toy site is running')
-})
+app.get("/", (req, res) => {
+  res.send("Musical toy site is running");
+});
 
 app.listen(port, () => {
-  console.log(`Musical toy listening on port ${port}`)
-})
+  console.log(`Musical toy listening on port ${port}`);
+});
